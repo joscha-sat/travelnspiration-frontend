a53b94ed1809932b1e9188535a1f38c36cd12d4d
joscha-sat <joscha.sattler@web.de>
2022-06-10 23:39:11 +0200

    theme with angular material

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,29 +1,14 @@
-import { AfterViewInit, Component } from '@angular/core';
-import { DarkmodeService } from '../../services/darkmode.service';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-navigation',
     templateUrl: './navigation.component.html',
     styleUrls: ['./navigation.component.scss'],
 })
-export class NavigationComponent implements AfterViewInit {
-    private darkTheme: boolean;
-    constructor(private darkmodeService: DarkmodeService) {}
+export class NavigationComponent {
+    constructor() {}
 
     darkmodeToggle() {
-        this.darkmodeService.darkModeToggle();
-        this.darkTheme = !this.darkTheme;
-    }
-
-    //Init
-    ngAfterViewInit(): void {
-        const darkMode: string | null = localStorage.getItem('darkMode');
-        if (darkMode === 'enabled') {
-            this.darkmodeService.enableDarkMode();
-            this.darkTheme = true;
-        } else {
-            this.darkmodeService.disableDarkMode();
-            this.darkTheme = false;
-        }
+        document.body.classList.toggle('lightTheme');
     }
 }
