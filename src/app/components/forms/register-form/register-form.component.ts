3bae7a33c812095a7056719b4bf2c06fbd95468e
joscha-sat <joscha.sattler@web.de>
2022-06-28 16:41:00 +0200

    register works

--- src/app/components/forms/register-form/register-form.component.ts
+++ src/app/components/forms/register-form/register-form.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../../services/auth.service';
+import { CreateUser } from '../../../interfaces/create-user';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-register-form',
@@ -10,9 +13,28 @@ export class RegisterFormComponent implements OnInit {
     hide = true;
     form: FormGroup;
 
-    constructor(private formBuilder: FormBuilder) {}
+    constructor(
+        private formBuilder: FormBuilder,
+        private authService: AuthService,
+        private router: Router
+    ) {}
 
-    onSubmit() {}
+    onSubmit() {
+        if (this.form.invalid) {
+            return;
+        }
+
+        const createUser: CreateUser = {
+            username: this.form.value.username,
+            email: this.form.value.email,
+            password: this.form.value.password,
+        };
+
+        this.authService.register(createUser).subscribe((res) => {
+            console.log(res);
+            this.router.navigate(['/login']).then();
+        });
+    }
 
     ngOnInit(): void {
         this.form = this.formBuilder.group({
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CreateUser } from '../interfaces/create-user';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class AuthService {
+    baseURL = 'http://localhost:3000/auth/';
 
-  constructor() { }
+    constructor(private http: HttpClient) {}
+
+    register(createUser: CreateUser) {
+        return this.http.post(this.baseURL + 'register', createUser);
+    }
 }
