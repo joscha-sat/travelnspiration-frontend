cf7919783b46f601a6e096399f87e4e0415cddb4
joscha-sat <joscha.sattler@web.de>
2022-07-06 15:30:52 +0200

    translation

---
+++ src/app/components/a-custom-components/base-title/base-title.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'app-base-title',
+    templateUrl: './base-title.component.html',
+    styleUrls: ['./base-title.component.scss'],
+})
+export class BaseTitleComponent {
+    @Input() ngTranslate: string;
+
+    constructor() {}
+}
